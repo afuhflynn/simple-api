871f746d151a36188d2cee6275b8027b8e059148
afuhflynn <flyinnsafuh@gmail.com>
2025-08-27 18:51:43 +0100

    feat: update utils readData function to handle errors

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,11 +6,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const readData = async () => {
-  const result = await readFile(path.join(__dirname, "..", "db", "data.json"), {
-    encoding: "utf-8",
-  });
-  const data = JSON.parse(result);
-  return data;
+  try {
+    const result = await readFile(
+      path.join(__dirname, "..", "db", "data.json"),
+      {
+        encoding: "utf-8",
+      }
+    );
+    const data = JSON.parse(result);
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 };
 
 export const writeData = async (data) => {
